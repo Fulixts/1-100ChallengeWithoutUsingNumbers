573c153682a60cbde299ffdd412c31efd9f87ae3
Gabriel Fuli <gabriel.fuli@capgemini.com>
2022-02-04 02:25:18 -0300

    Challenge 1-100 Without Use Numbers

---
+++ Challenge1-100WithoutUseNumbers/Challenge1-100WithoutUseNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Challenge1_100WithoutUseNumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //First Way
+
+            Console.WriteLine("First Way");
+
+            var one = ".";
+            var ten = "..........";
+
+            for (int i = one.Length; i <= (ten.Length * ten.Length); i++)
+                Console.WriteLine(i);
+
+            //Second Way
+
+            Console.WriteLine("\nSecond Way");
+
+            var x = "";
+            var z = x.Length;
+            var first = true;
+
+            z++;
+
+            if (first)
+            {
+                x = z.ToString() + x.Length.ToString() + x.Length.ToString();
+                first = false;
+                while (z <= int.Parse(x))
+                {
+                    Console.WriteLine(z);
+                    z++;
+                }
+            }
+        }
+    }
+}
